3c90aee394190375fb52b685ec4724be52d15c76
deck-adobe <deckreyes@adobe.com>
2024-09-09 07:16:53 -0700

    Added the banner block

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -142,7 +142,7 @@ export default async function decorate(block) {
   const hamburger = document.createElement('div');
   hamburger.classList.add('nav-hamburger');
   hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
-      <span class="nav-hamburger-icon"></span><span class="hamburger-text">Shop By Flower</span>
+    
     </button>`;
   hamburger.addEventListener('click', () => toggleMenu(nav, navSections));
   nav.prepend(hamburger);
